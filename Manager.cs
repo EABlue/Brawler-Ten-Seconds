ede82ad43d25bc12a37854abdbb1a1ea95b9c01c
EABlue <47250940+EABlue@users.noreply.github.com>
2019-02-01 10:55:55 -0600

    Add files via upload

---
+++ Manager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Counter() {
+        yield return new WaitForSeconds(10f);
+        foreach (GameObject Ob in GameObject.FindGameObjectsWithTag("Player")) {
+            Ob.GetComponent<PlayerScript>().ChangeCharacter;
+        }
+    }
+}
