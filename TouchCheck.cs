d757c983f0f2cf2a2215a37e84d1f34289e64780
EABlue <47250940+EABlue@users.noreply.github.com>
2019-02-01 09:45:23 -0600

    Add files via upload

---
+++ TouchCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchCheck : MonoBehaviour {
+
+    public MovementScript Move;
+    bool touching;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move.touching = touching;
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        touching = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        touching = false;
+    }
+}
